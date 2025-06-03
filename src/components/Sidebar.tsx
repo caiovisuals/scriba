ef7471cca249381d7bd6853bba9c0aac73a65b6b
caiothevisual <68815383+caiovisuals@users.noreply.github.com>
2025-06-03 12:41:50 -0300

    alguns erros do git

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,4 +2,4 @@ export default function Sidebar() {
     return (
         <div className="w-[10%] h-screen flex flex-col"></div>
     )
-}git remote set-url origin https://github.com/caiovisuals/scriba.git
+}
